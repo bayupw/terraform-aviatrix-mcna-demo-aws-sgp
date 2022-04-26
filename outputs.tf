444bf35be98c1ca5389cb7edca1740e008528ad9
bayupw <bayupw@gmail.com>
2022-04-27 09:33:09 +1200

    update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,23 +1,23 @@
 output "aws_sgp_prod02_instance_id" {
-  description = "AWS SGP Prod02 Instance"
+  description = "AWS Singapore Prod02 Instance"
   value       = module.aws_sgp_prod02_instance.aws_instance.id
   sensitive   = false
 }
 
 output "aws_sgp_prod02_ssm_session" {
-  description = "AWS SGP Prod02 Instance"
+  description = "AWS Singapore Prod02 Instance"
   value       = "aws ssm start-session --region ${data.aws_region.current.name} --target ${module.aws_sgp_prod02_instance.aws_instance.id}"
   sensitive   = false
 }
 
 output "aws_sgp_dev01_instance_id" {
-  description = "AWS SGP Dev01 Instance"
+  description = "AWS Singapore Dev01 Instance"
   value       = module.aws_sgp_dev01_instance.aws_instance.id
   sensitive   = false
 }
 
 output "aws_sgp_dev01_ssm_session" {
-  description = "AWS SGP Dev01 Instance"
+  description = "AWS Singapore Dev01 Instance"
   value       = "aws ssm start-session --region ${data.aws_region.current.name} --target ${module.aws_sgp_dev01_instance.aws_instance.id}"
   sensitive   = false
 }
