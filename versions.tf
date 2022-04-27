aa1fe1359e94f3ba4300064aa738fb6084fb0c91
bayupw <bayupw@gmail.com>
2022-04-27 13:17:12 +1200

    update version

--- versions.tf
+++ versions.tf
@@ -12,18 +12,3 @@ terraform {
     }
   }
 }
-
-provider "aws" {
-  alias  = "singapore"
-  region = "ap-southeast-1"
-}
-
-provider "aws" {
-  alias  = "sydney"
-  region = "ap-southeast-2"
-}
-
-provider "google" {
-  project = "bwibowo-01"
-  region  = "australia-southeast1"
-}
